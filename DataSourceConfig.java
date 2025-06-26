417ba40af339104b29b6bbaac36ad9ce4c103b12
chris Taylor <ctay456a@gmail.com>
2025-06-26 18:13:20 -0400

    Create DataSourceConfig.java

---
+++ DataSourceConfig.java
@@ -0,0 +1,46 @@
+package com.synergisticit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class DataSourceConfig {
+
+    private final Environment env;
+
+    public DataSourceConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName(env.getProperty("driverClassName"));
+        ds.setUrl(env.getProperty("url"));
+        ds.setUsername(env.getProperty("username"));
+        ds.setPassword(env.getProperty("password"));
+        return ds;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource());
+        emf.setPackagesToScan("com.synergisticit.domain");
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties props = new Properties();
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        props.setProperty("hibernate.hbm2ddl.auto", "update");
+
+        emf.setJpaProperties(props);
+        return emf;
+    }
+}
